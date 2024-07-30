71b9362581ac24f198740e141a17211871407b0b
MichelMulatoo <michel.mulato.mulato@gmail.com>
2024-07-29 21:50:43 -0400

    Actualizado

--- main.py
+++ main.py
@@ -7,11 +7,26 @@
 # else:
 #     print('Fin del ciclo while')
 
-minimo = 1
-contador = 7
+# minimo = 1
+# contador = 7
+#
+# while contador >= minimo:
+#     print(contador)
+#     contador -= 1
+# else:
+#     print('Fin ciclo while  ')
 
-while contador >= minimo:
-    print(contador)
-    contador -= 1
-else:
-    print('Fin ciclo while por ')
+
+altura = int(input("Ingresa la altura del triangulo: "))
+
+i = 1
+j = 1
+for i in range(altura):
+    for j in range(i):
+        print(" * ", end = "")
+    print(" ")
+
+for i in range(altura):
+    for j in range(altura-i):
+        print(" * ",end = "")
+    print(" ")
