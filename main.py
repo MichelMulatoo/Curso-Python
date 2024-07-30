cb8814db415d57e0f3470ca9afd75fc13b01da9e
MichelMulatoo <michel.mulato.mulato@gmail.com>
2024-07-30 11:42:06 -0400

    Leccion04

--- main.py
+++ main.py
@@ -17,16 +17,45 @@
 #     print('Fin ciclo while  ')
 
 
-altura = int(input("Ingresa la altura del triangulo: "))
+# altura = int(input("Ingresa la altura del triangulo: "))
+#
+# i = 1
+# j = 1
+# for i in range(altura):
+#     for j in range(i):
+#         print(" * ", end = "")
+#     print(" ")
+#
+# for i in range(altura):
+#     for j in range(altura-i):
+#         print(" * ",end = "")
+#     print(" ")
 
-i = 1
-j = 1
-for i in range(altura):
-    for j in range(i):
-        print(" * ", end = "")
-    print(" ")
+#
+# cadena = 'Hola Amor'
+#
+# for letra in cadena:
+#     print(letra)
+# else:
+#     print('Fin de ciclo for')
+#
 
-for i in range(altura):
-    for j in range(altura-i):
-        print(" * ",end = "")
-    print(" ")
+# for letra in 'Holanda':
+#     if letra == 'a':
+#         print(f'Letra encontrada: {letra}')
+#         break
+# else:
+#     print('Fin del ciclo for')
+#
+# for i in range(6):
+#     if i%2==0:
+#         print(f'Valor: {i}')
+
+# #solo se imprime el numero par
+# for i in range(6):
+#     if i % 2 != 0:
+#         continue#palabra continue sirve para ejecutar la siguiente iteracion dentro de nuestro ciclo
+#     print(f'Valor: {i}')
+
+# Listas es una conjunto de elementos
+
